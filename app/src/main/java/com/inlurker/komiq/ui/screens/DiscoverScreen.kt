0e6659390ba443c75995e28065ae1f35ac6246c2
InLurker <82582556+InLurker@users.noreply.github.com>
2023-06-05 12:25:16 +0700

    fixed nestedlazy issue

--- app/src/main/java/com/inlurker/komiq/ui/screens/DiscoverScreen.kt
+++ app/src/main/java/com/inlurker/komiq/ui/screens/DiscoverScreen.kt
@@ -1,6 +1,8 @@
 package com.inlurker.komiq.ui.screens
 
 import android.annotation.SuppressLint
+import android.content.Context
+import android.graphics.BitmapFactory
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -14,10 +16,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.GridItemSpan
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
@@ -36,18 +41,24 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.blur
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.zIndex
+import androidx.palette.graphics.Palette
 import com.inlurker.komiq.R
 import com.inlurker.komiq.ui.screens.components.LargeTopAppBarComponent
 import com.inlurker.komiq.ui.screens.components.SortingToolbar
@@ -57,6 +68,9 @@ import com.inlurker.komiq.ui.screens.components.SortingToolbar
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun DiscoverScreen() {
+
+    val context = LocalContext.current
+
     val sortingMethods = listOf("Last Updated", "Date Added")
 
     var searchQuery by remember { mutableStateOf("") }
@@ -79,32 +93,32 @@ fun DiscoverScreen() {
             .nestedScroll(scrollBehavior.nestedScrollConnection)
             .background(MaterialTheme.colorScheme.background)
     ) { paddingValue ->
-        LazyVerticalGrid(
-            columns = GridCells.Fixed(3),
-            horizontalArrangement = Arrangement.spacedBy(10.dp),
-            verticalArrangement = Arrangement.spacedBy(10.dp),
-            modifier = Modifier
-                .padding(paddingValue)
-                .padding(horizontal = 16.dp)
-        ) {
-            item(span = { GridItemSpan(3) }) {
-                DockedSearchBar(
-                    query = searchQuery,
-                    onQueryChange = { query -> searchQuery = query }, // Update the search query
-                    onSearch = { searchedAction = true },
-                    active = false,
-                    onActiveChange = { }, // Update the search activation
-                    placeholder = { Text(searchHint) },
-                    leadingIcon = {
-                        Icon(
-                            Icons.Default.Search,
-                            contentDescription = null,
-                            tint = MaterialTheme.colorScheme.onSurface
-                        )
-                    }
-                ) {}
-            }
-            if (searchedAction) {
+        if (searchedAction) {
+            LazyVerticalGrid(
+                columns = GridCells.Fixed(3),
+                horizontalArrangement = Arrangement.spacedBy(10.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier
+                    .padding(paddingValue)
+                    .padding(horizontal = 16.dp)
+            ) {
+                item(span = { GridItemSpan(3) }) {
+                    DockedSearchBar(
+                        query = searchQuery,
+                        onQueryChange = { query -> searchQuery = query }, // Update the search query
+                        onSearch = { searchedAction = true },
+                        active = false,
+                        onActiveChange = { }, // Update the search activation
+                        placeholder = { Text(searchHint) },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Default.Search,
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.onSurface
+                            )
+                        }
+                    ) {}
+                }
                 item(span = { GridItemSpan(3) }) {
                     SortingToolbar(
                         sortingMethods = sortingMethods,
@@ -119,34 +133,63 @@ fun DiscoverScreen() {
                         }
                     )
                 }
-            } else {
-                item(span = { GridItemSpan(3) }) {
+            }
+        } else {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(10.dp),
+                modifier = Modifier
+                    .padding(paddingValue)
+                    .padding(horizontal = 16.dp)
+            ) {
+                item {
+                    DockedSearchBar(
+                        query = searchQuery,
+                        onQueryChange = { query -> searchQuery = query }, // Update the search query
+                        onSearch = { searchedAction = true },
+                        active = false,
+                        onActiveChange = { }, // Update the search activation
+                        placeholder = { Text(searchHint) },
+                        leadingIcon = {
+                            Icon(
+                                Icons.Default.Search,
+                                contentDescription = null,
+                                tint = MaterialTheme.colorScheme.onSurface
+                            )
+                        }
+                    ) {}
+                }
+                item {
                     Column {
                         Text(
                             text = "Popular Now",
                             fontSize = 18.sp,
                             fontWeight = FontWeight.Medium
                         )
-                        Spacer(modifier = Modifier.height(10.dp))
+                        Spacer(modifier = Modifier.height(4.dp))
                         LazyRow(
                             modifier = Modifier
                         ) {
                             item {
                                 ElevatedCard(
-                                    elevation = CardDefaults.elevatedCardElevation(5.dp),
+                                    elevation = CardDefaults.elevatedCardElevation(4.dp),
                                     colors = CardDefaults.elevatedCardColors(
                                         containerColor = MaterialTheme.colorScheme.background
                                     ),
                                     modifier = Modifier
                                         .width(310.dp)
                                         .height(160.dp)
-                                        .padding(5.dp)
+                                        .padding(8.dp)
                                 ) {
                                     Box(
                                         modifier = Modifier
                                             .background(MaterialTheme.colorScheme.background)
                                             .fillMaxSize()
                                     ) {
+                                        val drawableImageSource = R.drawable.apo
+                                        val dominantPalette =
+                                            createPalette(context, drawableImageSource).dominantSwatch!!
+                                        val tagColor = Color(dominantPalette.rgb)
+                                        val textColor = Color(dominantPalette.bodyTextColor)
                                         Row(
                                             modifier = Modifier
                                                 .fillMaxSize()
@@ -154,7 +197,7 @@ fun DiscoverScreen() {
                                                 .zIndex(1f)
                                         ) {
                                             Image(
-                                                painterResource(id = R.drawable.apo),
+                                                painterResource(id = drawableImageSource),
                                                 contentDescription = "cover",
                                                 contentScale = ContentScale.Crop,
                                                 modifier = Modifier
@@ -164,21 +207,56 @@ fun DiscoverScreen() {
                                             )
                                             Column(
                                                 modifier = Modifier
-                                                    .padding(10.dp)
+                                                    .padding(horizontal = 10.dp)
+                                                    .padding(top = 10.dp)
                                             ) {
                                                 Text(
                                                     text = "Hyuuga Natsu",
-                                                    fontSize = 10 .sp
+                                                    fontSize = 10.sp
                                                 )
                                                 Text(
                                                     text = "Kusuriya no Hitorigoto",
                                                     fontSize = 16.sp,
-                                                    fontWeight = FontWeight.SemiBold
+                                                    fontWeight = FontWeight.SemiBold,
+                                                    textAlign = TextAlign.Start,
+
+                                                )
+                                                Spacer(
+                                                    Modifier.height(4.dp)
+                                                )
+                                                val GenreList = listOf(
+                                                    "Comedy",
+                                                    "Drama",
+                                                    "Historical",
+                                                    "Medical",
+                                                    "Mystery"
+                                                )
+                                                Row(
+                                                    horizontalArrangement = Arrangement.spacedBy(3.dp),
+                                                    modifier = Modifier
+                                                        .wrapContentWidth(Alignment.Start)
+                                                ) {
+                                                    for (genre in GenreList) {
+                                                        PopularGenreTag(
+                                                            text = genre,
+                                                            backgroundColor = tagColor,
+                                                            textColor = textColor
+                                                        )
+                                                    }
+                                                }
+                                                Spacer(
+                                                    Modifier.height(4.dp)
+                                                )
+                                                Text(
+                                                    text = "Maomao, a young woman trained in the art of herbal medicine, is forced to work as a lowly servant in the inner palace. Though she yearns for life outside its perfumed halls, she isn’t long for a life of drudgery! Using her wits to break a “curse” afflicting the imperial heirs, Maomao attracts the attentions of the handsome eunuch Jinshi and is promoted to attendant food taster. But Jinshi has other plans for the erstwhile apothecary, and soon Maomao is back to brewing potions and…solving mysteries?!",
+                                                    fontSize = 7.sp,
+                                                    overflow = TextOverflow.Ellipsis,
+                                                    textAlign = TextAlign.Start
                                                 )
                                             }
                                         }
                                         Image(
-                                            painter = painterResource(id = R.drawable.apo),
+                                            painter = painterResource(id = drawableImageSource),
                                             contentDescription = "cover",
                                             contentScale = ContentScale.Crop,
                                             modifier = Modifier
@@ -197,8 +275,31 @@ fun DiscoverScreen() {
     }
 }
 
+fun createPalette(context: Context, drawable: Int): Palette {
+    val bitmap = BitmapFactory.decodeResource(context.resources, drawable)
+    return Palette.from(bitmap).generate()
+}
 
-
+@Composable
+fun PopularGenreTag(
+    text: String,
+    backgroundColor: Color,
+    textColor: Color
+) {
+    Text(
+        text = text,
+        fontSize = 7.sp,
+        textAlign = TextAlign.Center,
+        color = textColor,
+        maxLines = 1,
+        modifier = Modifier
+            .background(
+                backgroundColor,
+                CircleShape
+            )
+            .padding(horizontal = 6.dp, vertical = 1.dp)
+    )
+}
 
 
 @Preview(showBackground = true)
